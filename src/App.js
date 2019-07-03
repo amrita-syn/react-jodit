035d4cb2abb43669f4c5c358e842811d1757631c
amrita-syn <amrita.yadav@synapsica.com>
2019-07-03 17:21:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'jodit';
+import 'jodit/build/jodit.min.css';
+import JoditEditor from "jodit-react";
+
+class App  extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'content',
+    };
+  };
+
+  updateContent = (value) => {
+    this.setState({ content: value })
+  }
+  /**
+   * @property Jodit jodit instance of native Jodit
+   */
+  jodit;
+  setRef = jodit => this.jodit = jodit;
+
+  config = {
+    readonly: false // all options from https://xdsoft.net/jodit/doc/
+  }
+  render() {
+    return (
+      <JoditEditor
+        editorRef={this.setRef}
+        value={this.state.content}
+        config={this.config}
+        onChange={this.updateContent}
+      />
+    );
+  }
 }
 
 export default App;
